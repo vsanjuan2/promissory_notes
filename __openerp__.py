5619e44772491df316a9a0f43afff1f0b342ff25
dontk1 <komolafetosin@gmail.com>
2018-05-30 20:29:30 +0100

    promissory_notes

---
+++ __openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# © 2018 Tosin Komolafe @ Ballotnet Solutions Ltd <komolafetosin@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name' : 'Chimpex Invoicing',
+    'version' : '1.0',
+    'author' : 'Chimpex Solutions Ltd',
+    'category' : 'Accounting & Finance',
+    'description' : """
+Chimpex Accounting and Financial Management.
+====================================
+
+Financial and accounting module that covers:
+--------------------------------------------
+    * General Accounting
+    * Cost/Analytic accounting
+    * Third party accounting
+    * Taxes management
+    * Budgets
+    * Customer and Supplier Invoices
+    * Bank statements
+    * Reconciliation process by partner
+
+Creates a dashboard for accountants that includes:
+--------------------------------------------------
+    * List of Customer Invoices to Approve
+    * Company Analysis
+    * Graph of Treasury
+
+Processes like maintaining general ledgers are done through the defined Financial Journals (entry move line or grouping is maintained through a journal) 
+for a particular financial year and for preparation of vouchers there is a module named account_voucher.
+    """,
+    'website': 'tosinkomolafe.com',
+    'depends' : ['account'],
+    'data': [
+        'account_invoice_view.xml',
+     
+    ],
+    'qweb' : [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ account_invoice.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# © 2018 Tosin Komolafe @ Ballotnet Solutions Ltd <komolafetosin@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api, _
+
+
+class account_invoice(models.Model):
+    _inherit = 'account.invoice'
+
+    payment_status = fields.Selection([
+            ('na','Na'),
+            ('pending','Pending'),
+            ('received','Received'),
+            ('pledged','Pledged'),
+            ('deposited','Deposited'),
+        ], string='Payment Status', 
+        default='na',
+        required=True)
+
+    promissory_note_number = fields.Char(
+        string='Promissory Note Number')
+
+    pledged_number = fields.Char(
+        string='Pledge Number')
+
+    pledged_date = fields.Date(
+        string='Pledge Date')
+
+    pledge_bank_id = fields.Many2one(
+        comodel_name='res.company', 
+        string='Pledge Bank')
+
+    payment_pledge = fields.Boolean(
+        string='Promissory note')
+
+    @api.multi
+    def onchange_payment_term_date_invoice(self, payment_term_id, date_invoice):
+        if not date_invoice:
+            date_invoice = fields.Date.context_today(self)
+        return {'value': {'date_due': self.date_due or date_invoice}}
